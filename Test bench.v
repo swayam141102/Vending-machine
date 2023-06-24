95e5457b194a0817904261254564c6b5fcade958
swayam141102 <127175745+swayam141102@users.noreply.github.com>
2023-06-24 22:02:46 +0530

    Test bench.v

--- Test bench.v
+++ Test bench.v
@@ -1,22 +1,27 @@
-module vending_machine_25_tb();
+module vmachine_tb();
   wire out;
-  wire [2:0] change;
+  wire [1:0] change;
   
   reg clk,rst;
-  reg [2:0]in;
+  reg [1:0]in;
    
-  vending_machine_25 dut(in,clk,rst,out,change);
+  vmachine dut(in,clk,rst,out,change);
   
   initial begin
     $monitor($time,"clk=%b,rst=%b,in=%b,out=%b,change=%b",clk,rst,in,out,change);
-    $dumpfile("vending_machine_25.vcd");
-    $dumpvars(0,vending_machine_25_tb);
-    
+   
     rst=1;
     clk=0;
     
     #6 rst=0;
-    in=2;
+    in=1;
     #11 in=2;
     #16 in=2;
+    #30 $finish;
+  end
+  always #5 clk=~clk;
+endmodule
+    
+    
+    
     
