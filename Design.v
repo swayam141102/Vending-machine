958e1384e684408f5d37c27c5cd69de9c76a082f
swayam141102 <127175745+swayam141102@users.noreply.github.com>
2023-06-24 22:02:11 +0530

    Design.v

--- Design.v
+++ Design.v
@@ -1,130 +1,87 @@
-module vending_machine_25(in,clk,rst,out,change);
-  input [2:0]in; // 000-0,001-5,010-10,011-15,100-20
+module vmachine(in,clk,rst,out,change);
   input clk,rst;
-  output reg [2:0]change; 
+  input [1:0] in;
   output reg out;
-  
-  parameter s0=3'b000;  
-  parameter s1=3'b001;  
-  parameter s2=3'b010; 
-  parameter s3=3'b011;
-  parameter s4=3'b100;
-  
-  reg [2:0] c_state,n_state;
-  
-  always @(posedge clk)
+  output reg [1:0] change;
+  parameter s0=2'b00;
+  parameter s1=2'b01;
+  parameter s2=2'b10;
+  reg [1:0] ps,ns;
+  always @( posedge clk)
     begin
-      if(rst==1)
+      if(rst)
         begin
-          c_state=0;
-          n_state=0;
-          change=3'b000;
+          ps<=s0;
+          change<=2'b00;
         end
       else
-        c_state=n_state;
-      
-      case(c_state)
-        s0:        // state s0 -- 0rs
-          if(in==0)
-            begin
-              n_state=s0;
-              out=0;
-              change=3'b000;
-            end
-        else if(in==3'b001)
-          begin
-            n_state=s1;
-            out=0;
-            change=3'b000;
-          end
-        else if(in==3'b010)
-          begin
-            n_state=s2;
-            out=0;
-            change=3'b000;
-          end
-        
-        s1:  // state s1 --5rs
-          if(in==0)
-            begin
-              n_state=s0;
-              out=0;
-              change=3'b001; //change returned 5rs
-            end
-        else if(in==3'b001)
-          begin
-            n_state=s2;
-            out=0;
-            change=3'b000;
-          end
-        else if(in==3'b010)
-          begin
-            n_state=s3;
-            out=0;
-            change=3'b000;
-          end
-        s2:     //state s2 -- 10rs
-          if(in==0)
-            begin
-              n_state=s0;
-              out=0;
-              change=3'b010;  // chnage returned 10rs
-end
-        else if(in==3'b001)
-          begin
-            n_state=s3;
-            out=0;
-            change=3'b000;
-          end
-        else if(in==3'b010)
-          begin
-            n_state=s4;
-            out=0;
-            change=3'b000;
-          end
-              
-              
-          s3:  // state s3--15rs
-             if(in==0)
-            begin
-              n_state=s0;
-              out=0;
-              change=3'b011;  // chnage returned 15rs
-            end
-        else if(in==3'b001)
-          begin
-            n_state=s4;
-            out=0;
-            change=3'b000;
-          end
-        else if(in==3'b010)
-          begin
-            n_state=s0;
-            out=1; //ouput =1 got 25rs
-            change=3'b000;
-          end
-             
-          s4:  // state s4--20rs
-               if(in==0)
-            begin
-              n_state=s0;
-              out=0;
-              change=3'b100;  // chnage returned 20rs
-             end
-            else if(in==3'b001)
-          begin
-            n_state=s0;
-            out=1;   ////ouput =1 got 25rs
-            change=3'b000;
-          end
-        else if(in==3'b010)
-          begin
-            n_state=s0;
-            out=1; //ouput =1 got 25rs
-            change=3'b010;
-          end
-        endcase
-              
+        ps<=ns;
     end
- endmodule
-            
+  always @(ps,in)
+    begin
+      case(ps)
+        s0:begin
+          if(in==0)
+            begin
+              ns=s0;
+              out=0;
+              change=2'b00;
+            end
+          else if(in==2'b01)
+            begin
+              ns=s1;
+              out=0;
+              change=2'b00;
+            end
+          else if(in==2'b10)
+            begin
+              ns=s2;
+              out=0;
+              change=2'b00;
+            end
+        end
+        s1:begin
+          if(in==0)
+            begin
+              ns=s0;
+              out=0;
+              change=2'b01;
+            end
+          else if(in==2'b01)
+            begin
+              ns=s2;
+              out=0;
+              change=2'b00;
+            end
+          else if(in==2'b10)
+            begin
+              ns=s0;
+              out=1;
+              change=2'b00;
+            end
+        end
+        
+       s2:begin
+          if(in==0)
+            begin
+              ns=s0;
+              out=0;
+              change=2'b10;
+            end
+          else if(in==2'b01)
+            begin
+              ns=s0;
+              out=1;
+              change=2'b00;
+            end
+          else if(in==2'b10)
+            begin
+              ns=s0;
+              out=1;
+              change=2'b01;
+            end
+        end
+        endcase
+    end
+endmodule
+        
